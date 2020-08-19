2a4a3546b5f5594cc629549178806b8c598160dc
HubertR <hubertcfc@gmail.com>
2020-08-19 20:58:22 +0200

    tabliceJednoImionaPobranie

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
-
+// tworzenie 5 tablic z imionami które sam podaje wyświetla się w liniach
 public class Main {
     public static void main(String[] args) {
-       int[] [] a = new int[10] [10];
-       a[5][5] = 10;
-       a[8][2] = 23;
-       for (int i = 0; i < a.length; i++) {
-
-           int[] b = a[i];
-           for (int j = 0; j < b.length; j++){
-               System.out.print(a[i][j]+ " ");
-           }
-           System.out.println();
-       }
+        String[] a = new String[5];
+        System.out.println("Tap a name :");
+        Scanner name = new Scanner(System.in);
+        for (int i = 0; i < 5; i++)
+        a[i] = name.nextLine();
+        for (int i = 0; i < 5; i++){
+            System.out.println("Cześć " + a[i]);
+        }
     }
 }
