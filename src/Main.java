5dfea7284e33fc635cf5f2174bc0240375459168
HubertR <hubertcfc@gmail.com>
2020-08-19 20:40:43 +0200

    tabliceWielowymiarowe

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+       int[] [] a = new int[10] [10];
+       for (int i = 0; i < a.length; i++) {
 
-        System.out.println("Hej jak masz na imię:");
-        String name = scan.nextLine();
-
-        System.out.println("Podaj swój wiek: ");
-        int age = scan.nextInt();
-
-
-        System.out.println("Ile zarabiasz: ");
-        double salary = scan.nextDouble();
-
-        System.out.println("Cześć " + name);
-        System.out.println("Masz " + age + " lat");
-        System.out.println("I zarabiasz "+ salary + " USD" );
+           int[] b = a[i];
+           for (int j = 0; j < b.length; j++){
+               System.out.println(a[i][j]);
+           }
+       }
     }
 }
