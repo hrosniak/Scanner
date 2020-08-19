f9a40b59be493f558772c62050c47b47be8b7092
HubertR <hubertcfc@gmail.com>
2020-08-19 20:43:39 +0200

    zmianaWarunków

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
        int[] [] a = new int[10] [10];
+       a[5][5] = 10;
+       a[8][2] = 23;
        for (int i = 0; i < a.length; i++) {
 
            int[] b = a[i];
            for (int j = 0; j < b.length; j++){
-               System.out.println(a[i][j]);
+               System.out.print(a[i][j]+ " ");
            }
+           System.out.println();
        }
     }
 }
