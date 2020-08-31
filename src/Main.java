440c6c1caf228bc9ead83f3c6a984191a45e5711
HubertR <hubertcfc@gmail.com>
2020-08-31 21:30:35 +0200

    instrukcjeWar

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,17 @@
 import java.util.Scanner;
-/* operatory bitowe
-0 false i true
-
-& iloczyn bitowy
-| suma bitowa
-^ XOR eXclusive OR
-x << 1 przesunięcie w lewo o 1
-x >> 2 przesunięcie w prawo o 2
-
-~ negacja bitowa
-
-0 0 0 1 posta na forum
-0 0 1 0 edytcja na forum
-0 1 0 0 usun posta
-1 0 0 0 ban
+/*
+Instrukcje warunkowe
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println(14&1);
+        int a = 5;
+        int b = 7;
+        int c = 9;
+
+        if (a > b) {
+            System.out.println("a > b");
+        }else
+            System.out.println("a <- b");
+        }
     }
-}
+
