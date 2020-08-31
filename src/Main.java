5018c7045c20c598d924671469c64aec57f56635
HubertR <hubertcfc@gmail.com>
2020-08-31 21:33:33 +0200

    zmiana

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,10 @@ public class Main {
             System.out.println("a > b");
         }else if (a < b) {
             System.out.println("a <- b");
+            System.out.println("Jesteś");
         }else
             System.out.println("a == b");
+            System.out.println("Albo i nie");
 
         }
 
