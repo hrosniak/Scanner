7989b5360f01937c819c141a0d7a231e6d031516
HubertR <hubertcfc@gmail.com>
2020-08-31 20:57:06 +0200

    Udemy

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
-// tworzenie 5 tablic z imionami które sam podaje wyświetla się w liniach
+
 public class Main {
     public static void main(String[] args) {
-        String[] a = new String[5];
-        System.out.println("Tap a name :");
-        Scanner name = new Scanner(System.in);
-        for (int i = 0; i < 5; i++)
-        a[i] = name.nextLine();
-        for (int i = 0; i < 5; i++){
-            System.out.println("Cześć " + a[i]);
+        int a = 5,
+                b = 5,
+                c = 7;
+
+        if (!(a == b) && c > a)
+            System.out.println("tak");
+        else {
+            System.out.println("No");
         }
     }
 }
