98ae0204445ca0bc19e7e82d8d23343aa27752f1
HubertR <hubertcfc@gmail.com>
2020-08-31 21:14:30 +0200

    negacjaBitowaPrzyklad

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,14 @@ x << 1 przesunięcie w lewo o 1
 x >> 2 przesunięcie w prawo o 2
 
 ~ negacja bitowa
+
+0 0 0 1 posta na forum
+0 0 1 0 edytcja na forum
+0 1 0 0 usun posta
+1 0 0 0 ban
  */
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(14&1);
     }
 }
