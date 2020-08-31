bc1fe9cce601ddb086407b848dbd934d01871bab
HubertR <hubertcfc@gmail.com>
2020-08-31 21:31:52 +0200

    zmiana

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,18 @@ Instrukcje warunkowe
 public class Main {
     public static void main(String[] args) {
         int a = 5;
-        int b = 7;
-        int c = 9;
+        int b = 5;
+
 
         if (a > b) {
             System.out.println("a > b");
-        }else
+        }else if (a < b) {
             System.out.println("a <- b");
+        }else
+            System.out.println("a == b");
+
         }
+
+
     }
 
